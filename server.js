e42ce942dadda0b4a6187ec4c927060b8e1f63ce
Timothy Baldock <tb@entropy.me.uk>
2013-08-15 22:53:21 +0100

    GET using ListingProvider + memory backed DB Reworking various functions in utils + fixing template bugs

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ var express    = require('express');
 var fs         = require("fs");
 var mustache   = require('mustache');
 var listing    = require('./lib/Listing.js');
+var ListingProvider = require('./lib/ListingProvider.js').ListingProvider;
+var listingProvider = new ListingProvider();
 var simutil    = require('./lib/SimUtil.js');
 var translator = require('./lib/Translator.js');
 var app = express();
@@ -82,32 +84,38 @@ app.post('/announce', function(req, res) {
     // TODO cope with proxy http header here
     listing.validate_dns(listing.parse_dns(req.body.dns), req.connection.remoteAddress,
         function () {
-            // Success callback
             var new_listing = new listing.Listing(req.body.dns, req.body.port);
 
-            var existing_listing = {};  // TODO get from object store
-            new_listing.update_from_object(existing_listing);
-            new_listing.update_from_body(req.body);
+            listingProvider.findById(new_listing.id, function (existing_listing) {
+                new_listing.update_from_object(existing_listing);
+                new_listing.update_from_body(req.body);
 
-            // Respond with just 202 Accepted header + single error code digit
-            // TODO replace with a better HTTP response given that we know if it worked now
-            res.writeHead(202, {"Content-Type": "text/html"});
-            res.end(JSON.stringify(new_listing));
-            //res.end("<a href=\"./list\">back to list</a>");
-
-            // TODO persist back to DB
+                // Respond with just 202 Accepted header + single error code digit
+                // TODO replace with a better HTTP response given that we know if it worked now
+                res.writeHead(202, {"Content-Type": "text/html"});
+                res.end(JSON.stringify(new_listing));
+                //res.end("<a href=\"./list\">back to list</a>");
+                listingProvider.save(new_listing, function () {
+                    console.log("Persisted new listing");
+                });
+            });
         }, function () {
-            // Failure callback
             res.send(400, "Bad Request - DNS field invalid");
-    });
+        }
+    );
 });
 
 app.get('/list', function(req, res) {
     "use strict";
-    var urlbase, get_times, pakset_names, paksets, paksets_mapped,
-        key, new_item, pakstring, csv_escape, response_text,
+    var urlbase, pakset_names, paksets, paksets_mapped,
+        key, new_item, pakstring, response_text,
         err;
     console.log("GET from " + req.connection.remoteAddress + " for " + req.url);
+
+    // TODO
+    // Rewrite this all using new Listings driver
+    // GET should show recently offline servers in red, and not return ones which
+    // have been offline for too long. Calculated dynamically during request
 
     // Process defaults
     if (!req.query.format) {
@@ -127,98 +135,69 @@ app.get('/list', function(req, res) {
             urlbase = urlbase + "?detail=" + req.query.detail;
         }
 
-        get_times = function (date, aiv) {
-            var cdate, last, offset, next, odue;
-            // Takes last report date and the announce interval and returns object containing information about times
-            // last - How long ago was the last report (and units for the time quantity)
-            // next - How long until the next report (and units)
-            // odue - How long overdue is the next report (and units)
-
-            cdate = (new Date()).getTime();
-
-            // Current minus last = ms since report
-            last  = cdate - date;
-
-            // Difference between last date + interval and now
-            offset = date + aiv * 1000 - cdate;
-
-            if (offset > 0) {
-                // Positive offset, not overdue
-                next  = offset;
-            } else if (offset === 0) {
-                // No offset, due now
-                odue  = 1;
-            } else {
-                // Negative offset, overdue
-                odue  = offset * -1;
-            }
-
-            return {last: last, next: next, odue: odue};
-        };
-
         // TODO - optimise this to only attach timing info for the expanded entry
 
-        pakset_names = [];
-        paksets = {};
-        for (key in listing.model) {
-            if (listing.model.hasOwnProperty(key)) {
-                new_item = listing.model[key];
-                pakstring = new_item.pak.split(" ")[0];
-                if (pakset_names.indexOf(pakstring) < 0) {
-                    // Add new pakset name
-                    pakset_names.push(pakstring);
-                    paksets[pakstring] = [];
+        // TODO online/offline
+        // online if it says it is IFF last report within announce interval
+        // offline otherwise
+        // Any entry found to be outside prune interval should be deleted
+        listingProvider.findAll(function (listings) {
+            var pakset_names = [];
+            var pakset_groups = {};
+            for (key in listings) {
+                if (listings.hasOwnProperty(key)) {
+                    var item = listings[key];
+                    var pakset_name = item.pak.split(" ")[0];
+                    if (pakset_names.indexOf(pakset_name) < 0) {
+                        pakset_names.push(pakset_name);
+                        pakset_groups[pakset_name] = [];
+                    }
+                    console.log(simutil.get_times(item.date, item.aiv));
+                    pakset_groups[pakset_name].push({
+                        detail: (key === req.query.detail),
+                        data: item,
+                        timing: simutil.get_times(item.date, item.aiv)
+                    });
                 }
-                paksets[pakstring].push({detail: (key === req.query.detail), data: new_item, timing: get_times(new_item.date, new_item.aiv)});
             }
-        }
-        // Map paksets into output format for mustache
-        paksets_mapped = [];
-        for (key in paksets) {
-            paksets_mapped.push({name: key, items: paksets[key]});
-        }
+            // Map paksets into output format for mustache
+            paksets_mapped = [];
+            for (key in pakset_groups) {
+                paksets_mapped.push({name: key, items: pakset_groups[key]});
+            }
 
-        res.write(mustache.to_html(templates["list.html"],
-            {lang: req.query.lang, translate: translate, timeformat: simutil.format_time,
-             paksets: paksets_mapped}));
+            res.write(mustache.to_html(templates["list.html"],
+                {lang: req.query.lang, translate: translate, timeformat: simutil.format_time,
+                 paksets: paksets_mapped}));
 
-        res.write(mustache.to_html(templates["footer.html"], {}));
-        res.end();
-
+            res.write(mustache.to_html(templates["footer.html"], {}));
+            res.end();
+        });
     } else if (req.query.format === "csv") {
-        csv_escape = function (text) {
-            while (text.indexOf("\"") !== -1) {
-                text = text.replace("\"", "");
-            }
-            if (text.indexOf(",") !== -1) {
-                text = "\"" + text + "\"";
-            }
-            return text;
-        };
+        var response = "";
 
-        response_text = "";
-
-        // TODO without a name field upload the dns/port field in its place
-        for (key in listing.model) {
-            if (listing.model.hasOwnProperty(key)) {
-                if (listing.model[key].dns
-                    && listing.model[key].port
-                    && listing.model[key].name 
-                    && listing.model[key].rev
-                    && listing.model[key].pak) {
-                    response_text = response_text
-                        + csv_escape(listing.model[key].name)
-                        + "," + csv_escape(listing.model[key].dns
-                        + ":" + listing.model[key].port)
-                        + "," + csv_escape(listing.model[key].rev.toString())
-                        + "," + csv_escape(listing.model[key].pak)
-                        + "," + csv_escape(listing.model[key].st.toString()) + "\n";
+        listingProvider.findAll(function (listings) {
+            for (key in listings) {
+                if (listings.hasOwnProperty(key)) {
+                    // TODO without a name field upload the dns/port field in its place
+                    if (listings[key].dns
+                        && listings[key].port
+                        && listings[key].name 
+                        && listings[key].rev
+                        && listings[key].pak) {
+                        response = response
+                            + csv_escape(listings[key].name)
+                            + "," + csv_escape(listings[key].dns
+                            + ":" + listings[key].port)
+                            + "," + csv_escape(listings[key].rev.toString())
+                            + "," + csv_escape(listings[key].pak)
+                            + "," + csv_escape(listings[key].st.toString()) + "\n";
+                    }
                 }
             }
-        }
-
-        res.writeHead(200, {"Content-Type": "text/plain", "Content-Length": response_text.length});
-        res.end(response_text);
+            res.writeHead(200, {"Content-Type": "text/plain", "Content-Length": response.length});
+            res.end(response);
+        });
     } else {
         err = "501 Not Implemented - The specified output format is not supported, supported formats are: " + available_formats.toString();
         res.writeHead(501, {"Content-Type": "text/html", "Content-Length": err.length});
